340655ebcb1d692ecc8efc9ac5e7b6c404a3a65b
Julian Squires <julian@cipht.net>
2016-07-08 13:54:20 -0400

    Make some CFLAGS more equal than others
    Because Gentoo.

--- c_src/build.sh
+++ c_src/build.sh
@@ -9,9 +9,8 @@ set -eu -o pipefail
 ERTS_INCLUDE_DIR=${ERTS_INCLUDE_DIR:-$(erl -noshell -s init stop -eval "io:format(\"~s/erts-~s/include/\", [code:root_dir(), erlang:system_info(version)]).")}
 ERL_INCLUDE_DIR=${ERL_INCLUDE_DIR:-$(erl -noshell -s init stop -eval "io:format(\"~s/usr/include/\", [code:root_dir()]).")}
 CC=${CC:-cc}
-CFLAGS=${CFLAGS:--fPIC -I${ERTS_INCLUDE_DIR} -I${ERL_INCLUDE_DIR} \
-  -std=gnu11 -O3 -march=native -mtune=native \
-  -ggdb -Wall -Wextra -Wno-missing-field-initializers}
+DEFAULT_CFLAGS="-O3 -march=native -mtune=native -ggdb -Wall -Wextra -Wno-missing-field-initializers"
+CFLAGS="-fPIC -I${ERTS_INCLUDE_DIR} -I${ERL_INCLUDE_DIR} -std=gnu11 ${CFLAGS:-$DEFAULT_CFLAGS}"
 LDFLAGS=${LDFLAGS:-}
 
 OS="$(uname -s)"
